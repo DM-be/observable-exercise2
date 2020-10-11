17b1b7daaf01ff2bd6f0f1b115fedc73acb5ab7e
DM-be <dennis.morent@student.howest.be>
2020-10-12 00:46:40 +0200

    2 files modified, 3 added

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Observable, Subscription } from "rxjs";
+
+interface Observer {
+  next: (value?: any) => void;
+  error?: (error: any) => void;
+  complete?: () => void;
+}
+@Component({
+  selector: "app-counter",
+  templateUrl: "./counter.component.html",
+  styleUrls: ["./counter.component.css"]
+})
+export class CounterComponent implements OnInit, OnDestroy {
+  public count: number = 0;
+  private incrementButton: HTMLButtonElement;
+  private decrementButton: HTMLButtonElement;
+  private incrementButtonClickSubscription: Subscription;
+  private decrementButtonClickSubscription: Subscription;
+
+  constructor() {}
+  ngOnDestroy(): void {
+    this.incrementButtonClickSubscription.unsubscribe();
+  }
+
+  ngOnInit() {
+    this.incrementButton = document.querySelector("#incrementBtn");
+    this.decrementButton = document.querySelector("#decrementBtn");
+    this.incrementButtonClickSubscription = this.observeIncrementButtonClick();
+    this.decrementButtonClickSubscription = this.observeDecrementButtonClick();
+  }
+
+
+
+  private observeIncrementButtonClick(): Subscription {
+
+    const observer: Observer = {
+      next: () => this.count++
+    };
+    const observable = new Observable(subscriber => {
+      this.incrementButton.onclick = () => subscriber.next();
+    });
+    const subscription = observable.subscribe(observer);
+    return subscription;
+  }
+
+  private observeDecrementButtonClick(): Subscription {
+    const observer: Observer = {
+      next: () => this.count--
+    };
+    const observable = new Observable(subscriber => {
+      this.decrementButton.onclick = () => subscriber.next();
+    });
+    const subscription = observable.subscribe(observer);
+    return subscription;
+  }
+
+}
