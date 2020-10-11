cd9235707c18e24dbb1be9d181ff60d626d050dd
DM-be <dennis.morent@student.howest.be>
2020-10-12 01:22:17 +0200

    1 file modified

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
 import { fromEvent, Observable, Subscription } from "rxjs";
+import { tap, throttleTime } from "rxjs/operators";
 
 interface Observer {
   next: (value?: any) => void;
@@ -31,18 +32,35 @@ export class CounterComponent implements OnInit, OnDestroy {
   }
 
 
-  // recreate the counter exercise using a creation operator
+  
+  private observableFromButtonClickEvent(button: HTMLButtonElement): Observable<Event> {
+    return fromEvent(button, 'click');
+  }
+
+
+  
   // .subscribe() also accepts an anonymous function, the first parameter is the next value, the next is error, and the last is complete.
   // usually only partial observers are used
   // rewrite this without an explicit observer object.
+
+  // after implementing, adjust the incrementButton to the following behavior:
+  // 1: prevent button spamming, don't allow more than one event per second through the stream 
+  // 2: log the value of the count variable, continue the chain
+  // 3: 
+
   private observeIncrementButtonClick(): Subscription {
-    const observable = fromEvent(this.incrementButton, 'click');
-    const subscription = observable.subscribe(() => this.count++);
+    const observable = this.observableFromButtonClickEvent(this.incrementButton);
+    const subscription = observable.pipe(
+      throttleTime(1000),
+      tap(() => console.log(this.count))
+    ).subscribe(() => this.count++)
+   // const subscription = observable.subscribe(() => this.count++);
     return subscription;
   }
 
+
   private observeDecrementButtonClick(): Subscription {
-    const observable = fromEvent(this.decrementButton, 'click');
+    const observable = this.observableFromButtonClickEvent(this.decrementButton);
     const subscription = observable.subscribe(() => this.count--);
     return subscription;
   }
